3a5bca5fb8a2567f2d24102e6c83a4376da3af65
Emanuel <itzemanuelj@gmail.com>
2024-06-15 23:29:07 -0400

    updated nav animation easter egg

--- script.js
+++ script.js
@@ -45,3 +45,19 @@ var intervalId = setInterval(function() {
         i++;
     }
 }, 700);
+
+function toggleNavbar() {
+  var x = document.getElementById("myNavbar");
+  if (x.className === "navbar") {
+      x.className += " responsive";
+  } else {
+      x.className = "navbar";
+  }
+  // Toggle the icon image
+  var icon = document.getElementById('nav-icon');
+  if (icon.src.includes('Hamburg.png')) {
+      icon.src = '/images/nav/Hamburg.png';
+  } else {
+      icon.src = '/images/nav/Hamburg2.png ';
+  }
+}
