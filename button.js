6a7548e458cfbe0deae3500eae7c24a5ca2122ce
Emanuel <itzemanuelj@gmail.com>
2024-06-30 16:18:45 -0400

    added scroll to up button on all pages

---
+++ button.js
@@ -0,0 +1,21 @@
+// script.js
+
+// Get the button
+let mybutton = document.getElementById("scrollUpBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+mybutton.onclick = function() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
